fa0c8f02c570eada1adade71d33b9f5ea8281195
junxian shen <sjx15@mails.tsinghua.edu.cn>
2018-01-31 18:24:21 +0800

    add crawl index of mainpage and restrictions

--- tutorial/tutorial/spiders/news_spider.py
+++ tutorial/tutorial/spiders/news_spider.py
@@ -9,5 +9,15 @@ class NewsSpider(scrapy.Spider):
 
     def parse(self, response):
         fn = "index.html"
+        originindex = '/publish/thunews/index.html'
+        englishindex = '/publish/thunewsen/index.html'
         with open(fn, "wb") as f:
             f.write(response.body)
+        for sel in response.xpath('//ul/li/a'):
+            title = sel.xpath('text()').extract();
+            link = sel.xpath('@href').extract();
+            if (link[0].find('publish') != -1 
+                and len(title) > 0
+                and link[0] != originindex
+                and link[0] != englishindex):
+                print title, link
